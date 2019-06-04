6bdbb93d85eb4373c8cef3ce2be5e5d09882dfcb
Harshal <harshal95iitk@gmail.com>
2019-06-04 09:57:17 +0530

    stack_bracket.py is added

---
+++ basics/bracket_stack.py
@@ -0,0 +1,19 @@
+S=input()
+
+def bracket(A):
+    count1,count2=0,0
+    flag = False
+    print(A)
+    for i in A:
+        if i=='(':
+            count1+=1
+        if i==')':
+            count2+=1
+        if count2 > count1:
+            flag = True
+
+    if count1==count2 and flag:
+        return True
+    else:
+        return False
+print(bracket(S))
