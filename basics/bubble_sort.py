7182e57f030838f48e000208604fe44fb05b5741
Harshal <harshal95iitk@gmail.com>
2019-06-12 18:13:49 +0530

    added

---
+++ basics/bubble_sort.py
@@ -0,0 +1,13 @@
+def BubbleSort(A):
+    for i in range(len(A)):
+        j = 0
+        while j < len(A)-1:
+            if A[j] > A[j+1]:
+                A[j], A[j+1] = A[j+1], A[j]
+            j+=1
+    return A
+
+if __name__=='__main__':
+    # seq = [5, 3, 8, 6, 7, 2]
+    seq = list(map(int, input().split()))
+    print(BubbleSort(seq))
