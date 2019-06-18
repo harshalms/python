295e0f90c0df416f98aaa166d7898fd4ead24ac4
Harshal <harshal95iitk@gmail.com>
2019-06-18 17:09:24 +0530

    decimal2binary added

---
+++ basics/deci2bin.py
@@ -0,0 +1,15 @@
+n = 8
+n = int(input())
+# c=[]
+s=''
+while n > 0:
+    # c.append(n%2)
+    s+=str(n%2)
+    n = n//2
+print( s[::-1])
+# j = len(c)-1
+# print(j)
+# while j > -1:
+#     print(c[j], end="")
+#     j-=1
+# print()
