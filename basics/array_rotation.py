d4eb8d660ede7524ead8d08d5d2fc9e7def12321
Harshal <harshal95iitk@gmail.com>
2019-06-25 22:51:59 +0530

    rotate array

--- basics/array_rotation.py
+++ basics/array_rotation.py
@@ -9,10 +9,16 @@ def rotate1(A, d):
     A.reverse()
     return A
 def rotate2(A, d):
-    b = A[:d]
-    c = A[d:]
-    A = c + b
+    A = A[d:] + A[:d]
+    return A
+def cylindrical_rotate(A, d):
+    for j in range(len(A)-d):
+        temp = A[-1]
+        for i in range(len(A)-1, 0, -1):
+            A[i] = A[i-1]
+        A[0] = temp
     return A
 
 print(rotate1(A, d))
 print(rotate2(A, d))
+print(cylindrical_rotate(A, d))
