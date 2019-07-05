319abc9f841e9ebd4c7fb8122dfccc0372884482
Harshal <harshal95iitk@gmail.com>
2019-07-05 20:47:21 +0530

    best insertion sort

--- basics/insertion_sort.py
+++ basics/insertion_sort.py
@@ -1,26 +1,26 @@
 # INSERTION SORT
+# def insertion_sort(A):
+#     s=[]
+#     s.insert(0, A[0])
+#     for i in range(1, len(A)):
+#         flag = False
+#         for j in range(i):
+#             if A[i] < s[j]:
+#                 s.insert(j, A[i])
+#                 flag = True
+#                 break
+#         if not flag:
+#             s.append(A[i])
+#             # if A[i]>s[j]:
+#             #     pos = i
+#     return s
 def insertion_sort(A):
-    s=[]
-    s.insert(0, A[0])
-    for i in range(1, len(A)):
-        flag = False
-        for j in range(i):
-            if A[i] < s[j]:
-                s.insert(j, A[i])
-                flag = True
-                break
-        if not flag:
-            s.append(A[i])
-            # if A[i]>s[j]:
-            #     pos = i
-    return s
-
-
-def insertion(A):
-    N = len(A)
-    for i in range(N):
-        
-        
+    for i in range(len(A)):
+        j=i
+        while j>0 and A[j]<A[j-1]:
+            A[j], A[j-1] = A[j-1], A[j]
+            j-=1
+    return A        
 
 if __name__ == "__main__":
     A = [74, 32, 32, 58, 64, 21, 64]
