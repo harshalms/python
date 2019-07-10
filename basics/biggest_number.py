23ff4092f4ce7f27879b9a727b9d658e0f8e9825
Harshal <harshal95iitk@gmail.com>
2019-07-10 21:30:16 +0530

    make biggest number

---
+++ basics/biggest_number.py
@@ -0,0 +1,26 @@
+'''GeeksForGeeks
+Arrange given numbers to form the biggest number | Set 1
+Given an array of numbers, arrange them in a way that yields 
+the largest value. For example, if the given numbers are {54, 546, 548, 60}, 
+the arrangement 6054854654 gives the largest value. And if the given numbers 
+are {1, 34, 3, 98, 9, 76, 45, 4}, then the arrangement 998764543431 gives the largest value.
+'''
+def bigNumber(A):
+    # A.sort()
+    for i in range(len(A)):
+        a = str(A[i])
+        A[i] = [int(a[0]), A[i]]
+    A.sort()
+            
+    A = A[::-1]
+    print(A)
+    big = str(A[0][1])
+    for i in range(1, len(A)):
+        if int(big+str(A[i][1])) < int(str(A[i][1])+big):
+            big = str(A[i][1])+big
+        else:
+            big = big+str(A[i][1])
+    return int(big)
+if __name__ == "__main__":
+    A = [1, 34, 3, 98, 9, 76, 45, 4]
+    print(bigNumber(A))
