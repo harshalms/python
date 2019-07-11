6c56d5fe179f4e316dee4341b7c8b1477ea85b22
Harshal <harshal95iitk@gmail.com>
2019-07-11 16:08:41 +0530

    biggest number

--- basics/biggest_number.py
+++ basics/biggest_number.py
@@ -6,14 +6,12 @@ the arrangement 6054854654 gives the largest value. And if the given numbers
 are {1, 34, 3, 98, 9, 76, 45, 4}, then the arrangement 998764543431 gives the largest value.
 '''
 def bigNumber(A):
-    # A.sort()
     for i in range(len(A)):
         a = str(A[i])
         A[i] = [int(a[0]), A[i]]
-    A.sort()
-            
+    A.sort()        
     A = A[::-1]
-    print(A)
+    # print(A)
     big = str(A[0][1])
     for i in range(1, len(A)):
         if int(big+str(A[i][1])) < int(str(A[i][1])+big):
