6c2df03a472ad2c4b6cc56e832494d4c655f7cc2
Harshal <harshal95iitk@gmail.com>
2019-07-15 19:37:28 +0530

    bubble sort

--- basics/bubble_sort.py
+++ basics/bubble_sort.py
@@ -4,10 +4,10 @@ def BubbleSort(A):
         while j < len(A)-1:
             if A[j] > A[j+1]:
                 A[j], A[j+1] = A[j+1], A[j]
+                # print(A)
             j+=1
     return A
-
 if __name__=='__main__':
-    # seq = [5, 3, 8, 6, 7, 2]
-    seq = list(map(int, input().split()))
+    seq = [5, 3, 8, 6, 7, 2]
+    # seq = list(map(int, input().split()))
     print(BubbleSort(seq))
