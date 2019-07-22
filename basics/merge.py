314a4638de39c3f0bd560de76602f790c926607b
Harshal <harshal95iitk@gmail.com>
2019-07-23 00:10:14 +0530

    Merge Sort

--- basics/merge.py
+++ basics/merge.py
@@ -18,10 +18,22 @@ def merge(A, B):
         j+=1
         
     return C
-
+def divide(A, start, end):
+        if end-start>1:
+            mid = (start+end)//2
+            k=divide(A, start, mid)
+            l=divide(A, mid, end)
+            return merge(k, l)
+        if end-start<=1:
+            return A[start:end]
 if __name__ == "__main__":
-    A=[[1, 2, 3,5], [1], [1, 6, 7]]
-    B=[[2, 4], [0], []]
-    for i in range(len(A)):
-        print("Input: \nA: %s \nB: %s" %(A[i], B[i]))
-        print("Output : %s\n---------------" % merge(A[i], B[i]))
+    A = [43, 32, 22, 78, 63, 57, 91, 13]
+    start, end = 0, len(A)
+    print(divide(A,start,end))
+
+# if __name__ == "__main__":
+#     A=[[1, 2, 3,5], [1], [1, 6, 7]]
+#     B=[[2, 4], [0], []]
+#     for i in range(len(A)):
+#         print("Input: \nA: %s \nB: %s" %(A[i], B[i]))
+#         print("Output : %s\n---------------" % merge(A[i], B[i]))
