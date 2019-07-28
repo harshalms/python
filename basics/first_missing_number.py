21f0f2278ad9fb4ca863208576b58cc4f7f26b1d
Harshal <harshal95iitk@gmail.com>
2019-07-28 18:14:00 +0530

    Finding first missing number

---
+++ basics/first_missing_number.py
@@ -0,0 +1,27 @@
+'''Interview-Bit
+First Missing Integer
+Asked in: Model N, InMobi, Amazon
+Given an unsorted integer array, find the first missing positive integer.
+Example:
+Given [1,2,0] return 3,
+[3,4,-1,1] return 2,
+[-8, -7, -6] returns 1
+Your algorithm should run in O(n) time and use constant space.'''
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def firstMissingPositive(self, A):
+        s = {}
+        for i in A:
+            s[i] = i
+        for i in range(1, len(A)+1):
+            if i not in s:
+                return i
+        return i+1
+a = Solution()
+b = Solution()
+c = Solution()
+print(a.firstMissingPositive([1,2,0]))
+print(b.firstMissingPositive([3,4,-1,1]))
+print(c.firstMissingPositive([-8, -7, -6]))
+
