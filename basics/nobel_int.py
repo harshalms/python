2db10fad308ec34fd3057348f41534c6c4bd8441
Harshal <harshal95iitk@gmail.com>
2019-07-31 21:16:42 +0530

    nobel integer

--- basics/nobel_int.py
+++ basics/nobel_int.py
@@ -1,4 +1,5 @@
 """InterviewBit
+Nobel Integer
 Given an integer array, find if an integer p exists in the array such 
 that the number of integers greater than p in the array equals to p
 If such an integer is found return 1 else return -1."""
